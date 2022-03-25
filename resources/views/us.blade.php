7b3a9e3cbe4652cef0699894db6ceb5390d9667f
antoniogiachin <giachinantonio@outlook.com>
2022-03-25 17:14:32 +0100

    fine esercizio con bonus

--- resources/views/us.blade.php
+++ resources/views/us.blade.php
@@ -16,9 +16,70 @@
             font-family: 'nunito';
         }
 
+        body{
+            height: 100vh;
+            background-color: #222;
+            color: lightgrey;
+            padding: 3rem 0;
+        }
+
+        h1{
+            text-align: center;
+            margin-bottom: 3rem;
+        }
+
+        .storia{
+            display: flex;
+            margin-bottom: 2rem;
+        }
+        
+        .storia-img img{
+            object-fit: contain;
+            margin-bottom: 1rem;
+        }
+        p{
+            font-size: 1.2em;
+            padding: 0 1rem;
+        }
+
+        button{
+            display: block;
+            padding: 6px 12px;
+            border: none;
+            background-color: transparent;
+            border: 3px solid lightgray;
+            margin: 0 5rem;
+        }
+
+        button a {
+            text-decoration: none;
+            color: lightgray;
+        }
+        .btn-container{
+            display: flex;
+            justify-content: center;
+        }
+
+        button:hover{
+            transform: scale(1.1);
+        }
+
     </style>
 </head>
 <body>
-    
+    <h1>{{$title}}</h1>
+    <div class="storia">
+        <p>{{$history}}</p>
+        <div class="storia-img">
+            @foreach ($imgs as $url => $alt)
+            <img src="{{$url}}" alt="{{$alt}}">
+            @endforeach
+        </div>
+    </div>
+    <div class="btn-container">
+        @foreach ($buttons as $key => $value)
+        <button><a href="{{$key}}">{{$value}}</a></button>
+        @endforeach
+    </div>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -90,3 +90,32 @@ Route::get('/faq', function () {
     ];
     return view('faq', $data);
 });
+
+//route per chi siamo
+Route::get('/us', function () {
+    
+    $data = [
+
+        "title" => 'La nostra storia',
+
+        "history" => "Google Inc. was officially launched in 1998 by Larry Page and Sergey Brin to market Google Search, which has become the most used web-based search engine. Larry Page and Sergey Brin, students at Stanford University in California, developed a search algorithm at first known as BackRub in 1996, with the help of Scott Hassan and Alan Steremberg. The search engine soon proved successful and the expanding company moved several times, finally settling at Mountain View in 2003. This marked a phase of rapid growth, with the company making its initial public offering in 2004 and quickly becoming one of the world's largest media companies. The company launched Google News in 2002, Gmail in 2004, Google Maps in 2005, Google Chrome in 2008, and the social network known as Google+ in 2011 (which was shut down in April 2019), in addition to many other products. In 2015, Google became the main subsidiary of the holding company Alphabet Inc. Google Inc. was officially launched in 1998 by Larry Page and Sergey Brin to market Google Search, which has become the most used web-based search engine. Larry Page and Sergey Brin, students at Stanford University in California, developed a search algorithm at first known as BackRub in 1996, with the help of Scott Hassan and Alan Steremberg. The search engine soon proved successful and the expanding company moved several times, finally settling at Mountain View in 2003. This marked a phase of rapid growth, with the company making its initial public offering in 2004 and quickly becoming one of the world's largest media companies. The company launched Google News in 2002, Gmail in 2004, Google Maps in 2005, Google Chrome in 2008, and the social network known as Google+ in 2011 (which was shut down in April 2019), in addition to many other products. In 2015, Google became the main subsidiary of the holding company Alphabet Inc.",
+
+        "imgs" => [
+
+            'https://picsum.photos/300'=> 'foto-1',
+            'https://picsum.photos/200'=> 'foto-2',
+            'https://picsum.photos/100'=> 'foto-3',
+
+        ],
+
+        "buttons" => [
+
+            "/" => 'Home',
+            "/doc" => 'Documentazione',
+            "/faq" => 'Faq',
+
+        ],
+
+    ];
+    return view('us', $data);
+});
